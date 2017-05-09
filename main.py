c5fa5a84c09a8bc2e44188fade5cd4367da5b2b3
vsr202vsr <vsr202vsr@gmail.com>
2017-05-09 10:07:17 +0530

    test

---
+++ main.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 15 10:31:01 2017
+
+@author: Reddy
+"""
+def parrot(voltage, state='a stiff', action='voom', type='Norwegian Blue'):
+    print("-- This parrot wouldn't", action, end=' ')
+    print("if you put", voltage, "volts through it.")
+    print("-- Lovely plumage, the", type)
+    print("-- It's", state, "!")
+#parrot(1000)                                          # 1 positional argument
+#==============================================================================
+# parrot(voltage=1000)                                  # 1 keyword argument
+# parrot(voltage=1000000, action='VOOOOOM')             # 2 keyword arguments
+# parrot(action='VOOOOOM', voltage=1000000)             # 2 keyword arguments
+#parrot('a million', 'bereft of life', 'jump')         # 3 positional arguments
+# parrot('a thousand', state='pushing up the daisies')  # 1 positional, 1 keyword
+#==============================================================================
+def main():
+    parrot('a million', 'bereft of life', 'jump')
